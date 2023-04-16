9dca3531796499f08d19991850112622ce6bb2db
Ft-silentknight <ft.silentknight@gmail.com>
2023-04-16 21:32:00 +0530

    Fourth Commit

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const progressContainer = document.getElementById("progress-container");
 const title = document.getElementById("title");
 const cover = document.getElementById("cover");
 
-const songs = ["Crimson Fly - Huma-Huma", "Indian Walk", "Silent Night"];
+const songs = ["Crimson_Fly", "Indian_Walk", "Silent_Night"];
 let songIndex = 1;
 
 function getSongTitle(song) {
